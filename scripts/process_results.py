97bc8953c459f1fc843a40879d43ad680084e73d
herswitz1 <hnorwitz@andrew.cmu.edu>
2022-04-05 10:34:04 -0400

    trying to add files from matpower to folder(4/4/22)

--- scripts/process_results.py
+++ scripts/process_results.py
@@ -7,14 +7,14 @@ def process_results(v, bus, Dense_eff, Sparse_eff):
     #4 bus case C:\Users\hersc\OneDrive\Desktop\matpower\matpower7.1\mp_r_4.mat
     #matpower_data = loadmat(r'')
     #14 Bus case C:\Users\hersc\OneDrive\Desktop\matpower\matpower7.1\mp_r_14.mat
-    #matpower_data = loadmat(r'testcases/result_14case')#this is for the 14 bus case
+    matpower_data = loadmat(r'testcases/result_14case')#this is for the 14 bus case
     #118 bus case C:\Users\hersc\OneDrive\Desktop\matpower\matpower7.1\mp_r_118.mat
-    matpower_data = loadmat(r'testcases/result_118case')
+    #matpower_data = loadmat(r'testcases/result_118case')
     ##getting the desired infromation out of file
     ###################################################
     ##IMPORTANT:EACH TIME CHANGINGING CHASE NEED TO ADJUST 'RESULT_14CASE' TO NEW CASE CONSIDERING
     ###################################################
-    mp_res = matpower_data['result_118case']['bus'][0,0]
+    mp_res = matpower_data['result_14case']['bus'][0,0]
     #putting desired information into vectors
     mp_res_mag = np.zeros(len(mp_res))
     mp_res_ang = np.zeros(len(mp_res))
